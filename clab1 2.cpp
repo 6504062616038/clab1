7b230ac570629696b3e818f754b2bd2666e6adc6
6504062616038 <153298618+6504062616038@users.noreply.github.com>
2024-11-28 18:03:13 +0700

    Add files via upload
    new version

--- clab1 2.cpp
+++ clab1 2.cpp
@@ -3,13 +3,14 @@
 int main() {
 	int n1;
 	int n2;
+	
 	scanf("%d%d",&n1,&n2);
 	
 	if (n1>n2){
-		printf("n1 is greater than n2");
+		printf("n1 > n2 = %d",n1-n2);
 	}
 	if (n2>n1){
-		printf("n2 is greater than n1");
+		printf("n2 > n1 = %d",n2-n1);
 	}
 	return 0;
 }
