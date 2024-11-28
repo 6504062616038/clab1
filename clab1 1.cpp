a381e0657dc0e0e0bea204e548471a66737c2255
6504062616038 <153298618+6504062616038@users.noreply.github.com>
2024-11-28 18:05:14 +0700

    Add files via upload

--- clab1 1.cpp
+++ clab1 1.cpp
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 int main() {
-	int n1 = 5;
-	int n2 = 10;
-	//scanf("%d%d",&n1,&n2);
+	int n1;
+	int n2;
+	
+	scanf("%d%d",&n1,&n2);
 	
 	if (n1>n2){
-		printf("n1 is greater than n2");
+		printf("n1 > n2 = %d",n1-n2);
 	}
-	else {
-		printf("n2 is greater than n1");
+	if (n2>n1){
+		printf("n2 > n1 = %d",n2-n1);
 	}
 	return 0;
 }
